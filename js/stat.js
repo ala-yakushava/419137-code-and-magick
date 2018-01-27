f2c58fbb01627bac14e0a52879ce5779daf5f720
linarsy <lina.stihi@gmail.com>
2018-01-27 16:25:35 +0300

    Сообщение о победе на канвас

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,17 @@ var renderCloud = function(ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+var renderText = function(ctx, text, x, y) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px, PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText(text, x, y);
+};
+
 window.renderStatistics = function(ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
+
+  renderText(ctx, 'Ура вы победили!', 150, 30);
+  renderText(ctx, 'Список результатов:', 150, 50);
 };
