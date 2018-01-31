55fb75437a6bcd4d48085dbe5dc20c3826582d9e
linarsy <lina.stihi@gmail.com>
2018-01-31 14:57:09 +0300

    Создала функцию генерации случайных данных

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var userDialog = document.querySelector(".setup");
-userDialog.classList.remove("hidden");
+document.querySelector(".setup").classList.remove("hidden");
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -10,24 +13,33 @@ var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [
   {
-    name: FIRST_NAMES[0],
+    name: FIRST_NAMES[0] + ' ' + LAST_NAMES[0],
     coatColor: COAT_COLOR[0],
     eyesColor: EYES_COLOR[0]
   },
   {
-    name: FIRST_NAMES[1],
+    name: FIRST_NAMES[1] + ' ' + LAST_NAMES[1],
     coatColor: COAT_COLOR[1],
     eyesColor: EYES_COLOR[1]
   },
   {
-    name: FIRST_NAMES[2],
+    name: FIRST_NAMES[2] + ' ' + LAST_NAMES[2],
     coatColor: COAT_COLOR[2],
     eyesColor: EYES_COLOR[2]
   },
   {
-    name: FIRST_NAMES[3],
+    name: FIRST_NAMES[3] + ' ' + LAST_NAMES[3],
     coatColor: COAT_COLOR[3],
     eyesColor: EYES_COLOR[3]
   }
 ];
 
+for (var i = 0; i < wizards.length; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+
+  similarListElement.appendChild(wizardElement);
+}
