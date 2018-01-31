18ca94b6a959b1cbd57dbafd81855c41d28617fa
linarsy <lina.stihi@gmail.com>
2018-01-31 14:16:05 +0300

    Создала массив персонажей

--- js/setup.js
+++ js/setup.js
@@ -8,3 +8,26 @@ var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вал
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var wizards = [
+  {
+    name: FIRST_NAMES[0],
+    coatColor: COAT_COLOR[0],
+    eyesColor: EYES_COLOR[0]
+  },
+  {
+    name: FIRST_NAMES[1],
+    coatColor: COAT_COLOR[1],
+    eyesColor: EYES_COLOR[1]
+  },
+  {
+    name: FIRST_NAMES[2],
+    coatColor: COAT_COLOR[2],
+    eyesColor: EYES_COLOR[2]
+  },
+  {
+    name: FIRST_NAMES[3],
+    coatColor: COAT_COLOR[3],
+    eyesColor: EYES_COLOR[3]
+  }
+];
+
