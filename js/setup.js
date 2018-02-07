dd863e82f2382178f3d9c4111d59e13b7f35565e
linarsy <lina.stihi@gmail.com>
2018-02-07 13:17:57 +0300

    Добавила обработчики для ввода с клавиатуры

--- js/setup.js
+++ js/setup.js
@@ -47,13 +47,45 @@ userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 // Открытие/закрытие окна настройки персонажа.
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
-var setupClose = userDialog.querySelector('.setup-close');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
 
 setupOpen.addEventListener('click', function() {
-  userDialog.classList.remove('hidden');
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
 });
 
 setupClose.addEventListener('click', function() {
-  userDialog.classList.add('hidden');
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
 });
