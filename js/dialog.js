3892a9cf1a151ea20db460ca49effc8288193ba4
linarsy <lina.stihi@gmail.com>
2018-02-19 18:52:37 +0300

    Перетаскивание

--- js/dialog.js
+++ js/dialog.js
@@ -1,11 +1,16 @@
 'use strict';
+
 // Открытие/закрытие окна настройки персонажа.
 
 (function () {
+  var SETUP_TOP = '80px';
+  var SETUP_LEFT = '50%';
+
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var inputName = setup.querySelector('.setup-user-name');
+  var dialogHandle = setup.querySelector('.setup-user-pic');
 
   var openPopup = function () {
     setup.classList.remove('hidden');
@@ -13,6 +18,8 @@
   };
 
   var closePopup = function () {
+    setup.style.top = SETUP_TOP;
+    setup.style.left = SETUP_LEFT;
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
   };
@@ -41,5 +48,43 @@
     window.util.isEscEvent(evt, function () {
       evt.stopPropagation();
     });
+  });
+
+// Перетаскивание диалога
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
--- js/setup.js
+++ js/setup.js
@@ -78,4 +78,42 @@
     fireball.style.background = FIREBALL_COLORS[defaultFireball];
     setup.querySelector('#fireball-color').value = FIREBALL_COLORS[defaultFireball];
   });
+
+// Перетаскивание предметов
+
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+  });
+
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var setupCell = artifactsElement.querySelectorAll('.setup-artifacts-cell');
+
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+  });
 })();
